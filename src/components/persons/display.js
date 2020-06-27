d5aa344593c6a24852002485e095c686582bd09d
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-06-27 23:16:42 +0300

    added find person

--- src/components/persons/display.js
+++ src/components/persons/display.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { gql, useLazyQuery } from '@apollo/client'
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone 
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`
 
 const Display = ({ persons }) => {
+  const [getPerson, result] = useLazyQuery(FIND_PERSON) 
+  const [person, setPerson] = useState(null)
+
+  const showPerson = (name) => {
+    getPerson({ variables: { nameToSearch: name } })
+  }
+
+  useEffect(() => {
+    if (result.data) {
+      setPerson(result.data.findPerson)
+    }
+  }, [result])
+
+  if (person) {
+    return(
+      <div>
+        <h2>{person.name}</h2>
+        <div>{person.address.street} {person.address.city}</div>
+        <div>{person.phone}</div>
+        <button onClick={() => setPerson(null)}>close</button>
+      </div>
+    )
+  }
+
   return (
     <>
       <h2>Persons</h2>
       {persons.map(p =>
         <div key={p.id}>
           {p.name} {p.phone}
+          <button onClick={() => showPerson(p.name)} >
+            show address
+          </button> 
         </div>  
       )}
     </>
