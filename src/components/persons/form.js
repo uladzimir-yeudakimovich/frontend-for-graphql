b93233eb15e56b87e5d6cdce443d3acafb9f340b
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-06-28 00:00:33 +0300

    added notification

--- src/components/persons/form.js
+++ src/components/persons/form.js
@@ -3,14 +3,17 @@ import { useMutation } from '@apollo/client'
 
 import { ALL_PERSONS, CREATE_PERSON } from '../queries'
 
-const PersonForm = () => {
+const PersonForm = ({ setError  }) => {
   const [name, setName] = useState('')
   const [phone, setPhone] = useState('')
   const [street, setStreet] = useState('')
   const [city, setCity] = useState('')
 
   const [ createPerson ] = useMutation(CREATE_PERSON, {
-    refetchQueries: [ { query: ALL_PERSONS } ]
+    refetchQueries: [ { query: ALL_PERSONS } ],
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message)
+    }
   })
 
   const submit = async (event) => {
--- src/components/persons/index.js
+++ src/components/persons/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 
+import Notification from '../notification'
 import Display from './display'
 import PersonForm from './form'
 import { ALL_PERSONS } from '../queries'
 
 const Persons = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS, {
     // pollInterval: 2000
   })
@@ -14,10 +17,18 @@ const Persons = () => {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
     <>
+      <Notification errorMessage={errorMessage} />
       <Display persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </>
   )
 }
